7211adde88a051ff024c4aa69320e52e98fe4df3
jozimar <jozimarlessa78@gmail.com>
2023-03-28 16:59:01 -0300

    ajuste

--- index.php
+++ index.php
@@ -9,9 +9,10 @@
 <body>
 <div>
      <?php
-    $n1 = 2;
-    $n2 = 3;
-    $m = $n1 + $n2 / 2;
+    $n1 = $_GET["a"];
+    $n2 = $_GET["b"];
+    echo "<h2>valores recebidos $n1 e $n2</h2>";
+    $m = ($n1 + $n2) / 2;
     echo " A soma vale " . ($n1+$n2);
     echo "<br/>A subtracao vale " . ($n1-$n2);
     echo "<br/>A multiplicacao vale " . ($n1*$n2);
