e27a2cea5140965b068e246c064a0f415eda6eeb
jozimar <jozimarlessa78@gmail.com>
2023-03-28 10:40:25 -0300

    ajuste

--- index.php
+++ index.php
@@ -4,12 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <?php
-    s
-    ?>
+    <title>Aritimetico</title>
 </head>
 <body>
-    
+<div>
+     <?php
+    $n1 = 2;
+    $n2 = 3;
+    $s = $n1 + $n2;
+    echo " A soma vale " . ($n1+$n2);
+    echo "<br/>A subtracao vale " . ($n1-$n2);
+    echo "<br/>A multiplicacao vale " . ($n1*$n2);
+    echo "<br/>A divisao vale " . ($n1/$n2);
+    echo "<br/>O modulo vale " . ($n1%$n2);
+    ?>
+</div>
 </body>
 </html>
